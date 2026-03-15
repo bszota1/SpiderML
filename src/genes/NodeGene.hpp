cb3cf180c81529da2effeb9c3997ef098458f184
bartekszota <szota.bartek@gmail.com>
2026-03-15 11:14:44 +0100

    single node class

---
+++ src/genes/NodeGene.hpp
@@ -0,0 +1,21 @@
+#pragma once 
+
+
+enum class NodeType{
+    INPUT,
+    BIAS,
+    HIDDEN,
+    OUTPUT
+};
+
+class NodeGene{
+public:
+    int nodeID;
+    NodeType nodeType;
+    float layerX;
+
+    NodeGene(int ID, NodeType type) : nodeID{ID}, nodeType{type}, layerX{0.0f}{}
+    NodeGene(int ID, NodeType type, float layerX) : nodeID{ID}, nodeType{type}, layerX{layerX}{}
+    NodeGene(const NodeGene& other) : nodeID{other.nodeID}, nodeType{other.nodeType}, layerX{other.layerX}{}
+    bool operator==(const NodeGene& other) const { return this->nodeID == other.nodeID; }
+};
